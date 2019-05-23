0a04492d73cd1a7374743131b1220d5cd760da7d
Felix <flixpar@gmail.com>
2019-05-23 12:54:25 -0400

    big improvements to args

--- args.py
+++ args.py
@@ -7,51 +7,47 @@ class Args:
 	###### Hyperparameters #######
 	##############################
 
-	epochs = 50                     # DEFAULT 50 (int 1-99)
-	batch_size = 4                  # DEFAULT 16 (int)
-	weight_decay = 1e-5             # DEFAULT 0 (float)
+	epochs = 100                    # DEFAULT 100 (int >1)
+	batch_size = 4                  # DEFAULT 4 (int >1)
+	weight_decay = 1e-5             # DEFAULT 1e-5 (float >=0)
 
-	arch = "resnet50"               # DEFAULT resnet50 (resnet50 | resnet152 | senet154 | inceptionv4)
+	arch = "inceptionv4"            # DEFAULT resnet50 (resnet50 | resnet152 | senet154 | inceptionv4)
 
-	initial_lr = 3e-5               # DEFAULT 1e-5 (float)
-	lr_schedule = None              # DEFAULT None (None | poly | exp | step | multistep | cosine)
+	initial_lr = 3e-5               # DEFAULT 3e-5 (float >0)
+	lr_schedule = "poly"            # DEFAULT poly (None | poly | exp | step | multistep | cosine)
 	lr_schedule_params = {          # DEFAULT {} (dict)
 	}
 
-	loss = "bce"                    # DEFAULT softmargin (bce | focal | fbeta | softmargin)
+	loss = "bce"                    # DEFAULT bce (bce | focal | fbeta | softmargin)
 	loss_params = {
-		"focal_gamma": 2,           # DEFAULT 2 (float)
-		"fbeta": 1                  # DEFAULT 1 (float)
 	}
 
-	weight_mode = None              # DEFAULT None ({inverse, sqrt} | None)
-	weight_method = None            # DEFAULT None (loss | sampling | None)
-
 	device_ids = [0]                # DEFAULT [0,] (list int 0-8)
-	workers = 4                     # DEFAULT 8 (int 0-16)
+	workers = 4                     # DEFAULT 4 (int >=0)
 
-	log_freq = 5                    # DEFAULT 5 (int)
-	n_val_samples = None            # DEFAULT None (int | None)
-	n_train_eval_samples = 64       # DEFAULT 64 (int | None)
+	log_freq = 5                    # DEFAULT 5 (int >0)
+	n_val_samples = None            # DEFAULT None (int >0 | None)
+	n_train_eval_samples = 64       # DEFAULT 64 (int >0 | None)
 
 	train_split = "train"           # DEFAULT train (train | val | trainval)
 	val_split   = "val"             # DEFAULT val (train | val | trainval)
 
-	img_size = 1024                 # DEFAULT None (None | int 224-4096)
+	img_size = 1024                 # DEFAULT 1024 (None | int 224-4096)
+
+	include_laser = True            # DEFAULT True (bool)
+	example_weighting = False       # DEFAULT False (bool)
 
 	train_augmentation = tfms.Compose([
 		tfms.HorizontalFlip(p=0.5),
 		tfms.VerticalFlip(p=0.5),
-		tfms.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=20),
-		tfms.RandomBrightnessContrast(),
-		tfms.GaussNoise(var_limit=(2, 8))
+		tfms.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=25),
 	])
 
 	##############################
 	########### Test #############
 	##############################
 
-	test_augmentation = [         # DEFAULT [] (list)
+	test_augmentation = [
 		tfms.HorizontalFlip(p=1.0),
 		tfms.VerticalFlip(p=1.0),
 	]
